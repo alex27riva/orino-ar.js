375ea64d16f2d81b5bdc4d163de56c54678882de
Alessandro Riva <alex27riva@gmail.com>
2022-10-07 10:34:41 +0200

    Some tests

--- places-from-json/script.js
+++ places-from-json/script.js
@@ -1,49 +1,31 @@
 // getting places from APIs
 function loadPlaces(position) {
-    const params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: '<your-client-id>',
-        clientSecret: '<your-client-secret>',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API (limit param: number of maximum places to fetch)
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=30 
-        &v=${params.version}`;
-    return fetch(endpoint)
+    return fetch('./Waypoints.geojson')
         .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
+            return res['feature'].json();
+        });
+    console.log(parsedWaypoints);
+    var features = parsedWaypoints['features'];
+    console.log("places loaded");
+
 };
 
 
 window.onload = () => {
+    console.log("page loaded");
+    
     const scene = document.querySelector('a-scene');
 
     // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
-
+        console.log(position.coords);
         // than use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
-
+            .then((features) => {
+                features.forEach((wp) => {
+                    const latitude = wp['geometry']['coordinates'][0];
+                    const longitude = wp['geometry']['coordinates'][0];
+                    console.log(latitude);
                     // add place name
                     const placeText = document.createElement('a-link');
                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
