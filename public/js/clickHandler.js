8586a59eba2fb2e61be93f2688d220548dff9d96
Alessandro Riva <alex27riva@gmail.com>
2023-02-19 18:25:57 +0100

    Fix clickhandler

--- public/js/clickHandler.js
+++ public/js/clickHandler.js
@@ -1,27 +1,28 @@
 AFRAME.registerComponent("clickhandler", {
-		init: function() {	
-			this.el.addEventListener("click", function(evt) {
-				let icon = evt.target || evt.srcElement;
-				let index = parseInt(icon.getAttribute("id"));
-				let panel = parent.document.getElementById("info-panel");
-				let el = parent.db.location[index];
-				panel.innerHTML = "";
-				let backBtn = parent.document.createElement("button");
-				backBtn.setAttribute("class", "button close");
-				backBtn.setAttribute("onClick", "btnClose()");
-				panel.appendChild(backBtn);
+	init: function () {
+		this.el.addEventListener("click", function (evt) {
+			let plane = evt.target || evt.srcElement;
+			let iconContainer = plane.parentNode;
+			let index = parseInt(iconContainer.getAttribute("id"));
+			let panel = parent.document.getElementById("info-panel");
+			let el = parent.db.location[index];
+			panel.innerHTML = "";
+			let backBtn = parent.document.createElement("button");
+			backBtn.setAttribute("class", "button close");
+			backBtn.setAttribute("onClick", "btnClose()");
+			panel.appendChild(backBtn);
+			
+			// Panel title
+			let title = parent.document.createElement("h1");
+			title.innerHTML = el.name;
+			panel.appendChild(title);
 
-				let name = parent.document.createElement("h1");
-				name.innerHTML = el.name;
-				panel.appendChild(name);
-				let title = parent.document.createElement("h2");
-				title.innerHTML = el.name;
-				let content = parent.document.createElement("div");
-				content.innerHTML = el.desc;
-				panel.appendChild(title);
-				panel.appendChild(content);
-				
-				parent.displayInfoPanel();
-			});
-		}
+			// Panel description
+			let description = parent.document.createElement("h2");
+			description.innerHTML = el.description;
+			panel.appendChild(description)
+
+			parent.displayInfoPanel();
+		});
+	}
 });
